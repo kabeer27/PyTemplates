374344f2ee95a89db08b7e200672db3b5ac9af33
Kabeer <kabeer.205@gmail.com>
2019-09-16 01:25:06 +0530

    Adding Sieving Algorithms

---
+++ Sieves/Count_Factors.py
@@ -0,0 +1,11 @@
+def countFactors(n):
+    ##Count number of factors of numbers from 1 - N
+    count_factor=[1]*(n+1)
+    for i in range(2,n+1):
+        for j in range(i,n+1,i):
+            count_factor[j]+=1
+    
+    return count_factor
+
+    
+    
---
+++ Sieves/Prime_Factor_Count.py
@@ -0,0 +1,14 @@
+def primeFactors(n):
+    ##Count number of prime factors of numbers from 2 - N , 1 is not counted as a prime number.
+    prime_factor_count=[0]*(n+1)
+    for i in range(2,n+1):
+        if prime_factor_count[i]==0:
+            p=i
+            while p<n+1:
+                prime_factor_count[p]+=1
+                p+=i
+    return prime_factor_count
+
+
+    
+    
